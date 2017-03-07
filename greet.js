80f328f93cd8ecfa7c9885924c2dd66b3370cd36
athdot <athdot@gmail.com>
2017-03-07 13:44:07 -0700

    Update greet.js

--- greet.js
+++ greet.js
@@ -5,15 +5,30 @@ var greet2 = "Hi"
 var greet3 = "'Sup!"
 var randgreetselector = Math.floor((Math.random() * 3) + 1);
 if(randgreetselector == 1){
- var greet = greet 1       
+ var greet = greet1       
 }
 if(randgreetselector == 2){
- var greet = greet 1       
+ var greet = greet2      
 }
 if(randgreetselector == 3){
- var greet = greet 1       
+ var greet = greet3    
 }
 //End Greets
+//goodbyes
+var bye1 = "'Bye"
+var bye2 = "Goodbye"
+var bye3 = "See ya later!"
+var randbyeselector = Math.floor((Math.random() * 3) + 1);
+if(randbyeselector == 1){
+ var bye = bye1       
+}
+if(randbyeselector == 2){
+ var bye = bye2       
+}
+if(randbyeselector == 3){
+ var bye = bye3    
+}
+//End Goodbyes
 
 //End subjects
 //Word Deffinitions
