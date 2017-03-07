b24adab30c72b1829de2ecc29413f6a3ec6684e5
athdot <athdot@gmail.com>
2017-03-07 11:31:46 -0700

    Create greet.js

---
+++ greet.js
@@ -0,0 +1,7 @@
+        var ifrm = document.createElement("iframe");
+        ifrm.setAttribute("src", "https://athdot.github.io/test/window.html");
+        var scwid = (screen.width)*(78.8/80)
+        ifrm.style.width = (scwid + "px");
+        ifrm.style.height = "100px";
+        ifrm.setAttribute("frameborder", "0");
+        document.body.appendChild(ifrm);
